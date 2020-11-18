5d4224df09ac9c56d08933263a4f7798d0b183ac
LaurenceB28 <laurence.brd@gmail.com>
2020-11-18 15:14:21 +0100

    exophp3P1

---
+++ index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exophp3</title>
+</head>
+<body>
+    <P><?php 
+    $km = 1 . ' - '; 
+    echo $km;
+    $km = 3 . ' - ';
+    echo $km;
+    $km = 125;
+    echo $km;?></P>
+</body>
+</html>
